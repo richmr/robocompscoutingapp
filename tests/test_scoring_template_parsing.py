3edd41570b3aefb9cccd8ce2517c00bf1514878c
Michael Rich <richmr2174@gmail.com>
2023-11-26 20:33:10 -0800

    delete some comments

--- tests/test_scoring_template_parsing.py
+++ tests/test_scoring_template_parsing.py
@@ -5,26 +5,6 @@ from robocompscoutingapp.ScoringPageParser import ScoringPageParser
 from robocompscoutingapp.AppExceptions import ScoringPageParseError, ScoringPageParseWarning
 from robocompscoutingapp.GlobalItems import ScoringClassTypes
 
-"""
-Needed tests:
-- detect match_selection div (pos/neg) - Not needed..  I'm just going to add it anyway
-- detect review_and_submit div (pos/neg) - Not needed..  I'm just going to add it anyway
-- detect error_modal div (pos/neg) - Not needed..  I'm just going to add it anyway
-- detect sending_data_modal div (pos/neg) - Not needed..  I'm just going to add it anyway
-
-- detect game_mode_group (pos/neg)
-- collect game_mode (return findings)
-    - Check for duplicates, error
-- collect score_tally (return findings)
-- collect score_flag (return findings)
-- detect click_feedback (return findings) - not sure this is needed.  Its window dressing
-- collect data-modename (return findings)
-    - Check for duplicates, error
-- collect data-scorename (return findings)
-    - Check for duplicates, error
-- collect data-onlyForMode (return findings)
-    - Check for modes that do not exist
-"""
 
 def test_scoring_class_parse():
     spp = ScoringPageParser("<div class='scoring'></div>")
