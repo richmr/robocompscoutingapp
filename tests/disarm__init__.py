16d59871f7dd5042e7809d5167b74628d2a4660a
Michael Rich <richmr2174@gmail.com>
2023-12-05 16:47:58 -0800

    Needed to ensure tests work.

--- tests/disarm__init__.py
+++ tests/disarm__init__.py
@@ -1,3 +1,9 @@
 # SPDX-FileCopyrightText: 2023-present Michael Rich <richmr2174@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+
+"""
+This needs to be disarmed to allow tests to work.
+Lots of discussions about this out there, and the answer varies.
+For me, "disarming" the __init__.py file helps.
+"""
