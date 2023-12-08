fba78a5788e9c02d03788828fa9f6dda2b13fc86
Michael Rich <richmr2174@gmail.com>
2023-12-07 20:38:37 -0800

    replace execute with scalars

--- src/robocompscoutingapp/UserHTMLProcessing.py
+++ src/robocompscoutingapp/UserHTMLProcessing.py
@@ -94,7 +94,7 @@ class UserHTMLProcessing:
         """
         try:
             # Find the object
-            page_result = self.dbsession.execute(select(ScoringPageStatus).filter_by(scoring_page_hash=self.getFileHash())).one()
+            page_result = self.dbsession.scalars(select(ScoringPageStatus).filter_by(scoring_page_hash=self.getFileHash())).one()
             # Return the current status
             return page_result              
         except NoResultFound:
